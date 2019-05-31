d9c073ece1dbab45da8ecb1252ef63e7be7bf561
8infy <99danilt@gmail.com>
2019-05-30 22:56:34 -0700

    Changed the parameters to be constant references.

--- include/SML/utils.h
+++ include/SML/utils.h
@@ -4,12 +4,12 @@
 
 namespace sml {
 
-	inline float to_radians(float degrees)
+	inline float to_radians(const float& degrees)
 	{
 		return degrees * SML_PI_OVER_180;
 	}
 
-	inline float to_degrees(float radians)
+	inline float to_degrees(const float& radians)
 	{
 		return radians * SML_180_OVER_PI;
 	}
