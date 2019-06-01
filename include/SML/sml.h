83dbb9d7e53e6246f1fea6c45e8a241deccd3428
8infy <99danilt@gmail.com>
2019-06-01 01:18:11 -0700

    Inlined the cross product function.

--- include/SML/sml.h
+++ include/SML/sml.h
@@ -26,7 +26,7 @@ namespace sml {
 		return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z + lhs.w * rhs.w;
 	}
 
-	vec3 cross(const vec3& lhs, const vec3& rhs)
+	inline vec3 cross(const vec3& lhs, const vec3& rhs)
 	{
 		return vec3(lhs.y * rhs.z - lhs.z * rhs.y,
                             lhs.z * rhs.x - lhs.x * rhs.z,
