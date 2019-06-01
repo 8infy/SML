d91de6be30420cda21193e9961151218428d3a8b
8infy <99danilt@gmail.com>
2019-06-01 01:14:59 -0700

    Fixed spacing for cross product.

--- include/SML/sml.h
+++ include/SML/sml.h
@@ -29,7 +29,7 @@ namespace sml {
 	vec3 cross(const vec3& lhs, const vec3& rhs)
 	{
 		return vec3(lhs.y * rhs.z - lhs.z * rhs.y,
-					lhs.z * rhs.x - lhs.x * rhs.z,
-					lhs.x * rhs.y - lhs.y * rhs.x);
+                            lhs.z * rhs.x - lhs.x * rhs.z,
+                            lhs.x * rhs.y - lhs.y * rhs.x);
 	}
 }
