8a95c2bdfa289a4e47d0449fd97fd0c2251b3a04
8infy <99danilt@gmail.com>
2019-06-02 23:53:25 -0700

    Added orthographic and perspective projection matricies.

--- include/SML/mat4.h
+++ include/SML/mat4.h
@@ -39,6 +39,38 @@ namespace sml {
 			return mat4(1.0f);
 		}
 
+		static mat4 orthographic(float left, float right, float bottom, float top, float near, float far)
+		{
+			mat4 result(1.0f);
+
+			result[0][0] =  2.0f / (right - left);
+			result[1][1] =  2.0f / (top - bottom);
+			result[2][2] = -2.0f / (far - near);
+
+			result[3][0] = -(right + left) / (right - left);
+			result[3][1] = -(top + bottom) / (top - bottom);
+			result[3][2] = -(far + near) / (far - near);
+
+			return result;
+		}
+
+		static mat4 perspective(float fov, float aspectRatio, float near, float far)
+		{
+			mat4 result(0.0f);
+
+			float tanHalfFov = tan(to_radians(fov * 0.5f));
+
+			result[0][0] = 1 / (aspectRatio * tanHalfFov);
+			result[1][1] = 1 / tanHalfFov;
+			result[2][2] = -(far + near) / (far - near);
+
+			result[2][3] = -1;
+			
+			result[3][2] = -(2 * far * near) / (far - near);
+
+			return result;
+		}
+
 		vec4& operator[](int column)
 		{
 			return columns[column];
